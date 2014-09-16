d54681a2d7a4a4e8427853161f573e4d9e26d3e2
kdinev <kdinev@mail.bw.edu>
2014-09-16 11:15:06 +0300

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				files: {
+					'build/calculator.min.js': ['src/calculator.js']
+				}
+			}
+		},
+		cssmin: {
+			combine: {
+				files: {
+					'build/css/calculator.min.css': ['src/css/calculator.css']
+				}
+			}
+		},
+		jshint: {
+			options: {
+				jshintrc: ".jshint",
+				ignores: ["build/*.min.js"]
+			},
+			//changed: [],
+			all: [
+				"Gruntfile.js",
+				"src/*.js",
+				"test/*.js",
+			]
+		},
+		qunit: {
+			all: ['test/*.html']
+		},
+		watch: {
+			files: ["**/*.js"],
+			tasks: ["test"],
+			options: {
+				spawn: false
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-qunit");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+
+	// Default task(s).
+	grunt.registerTask('default', ['jshint', 'uglify', 'cssmin']);
+	
+	// Custom task{s}.
+	grunt.registerTask("test", ["jshint"]);
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+CalculatorJS
+=========
+
+A JavaScript calculator and an expression parser.
+
+Description:
+
+Parses and evaluates mathematical expressions. The expression is provided to the expression parser as a string.
+
+The CalculatorJS is a calculator widget provided dependent on the EpsilonJS expression parser. This widget was created to test the epsilon expression parser. The calculator widget is dependent on jQuery and jQuery UI. In order to use it:
+    
+    <div id="calculator"></div>
+    <script type="text/javascript">
+		$(document).ready(function () {
+			$("#calculator").calculator();
+		});
+	</script>
+
+The calculator widget is used in a Windows 8.1 store application which you may download and play with here: [http://apps.microsoft.com/windows/app/epsilon-calculator/ec41ebdd-00c6-4654-a2a2-b297a0118a87](http://apps.microsoft.com/windows/app/epsilon-calculator/ec41ebdd-00c6-4654-a2a2-b297a0118a87)
